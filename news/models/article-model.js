ecd3593e8acb8b1e0bfb9831aa248faf933c6fa7
Idiris Mohamed <idirismohamed@Idiriss-MBP.communityfibre.co.uk>
2024-11-08 17:04:54 +0000

    Added a left join when searching with id which actually allows fetching articles with no comments

--- news/models/article-model.js
+++ news/models/article-model.js
@@ -4,7 +4,7 @@ const format = require("pg-format");
 function fetchArticleById(id) {
   return db
     .query(
-      `SELECT articles.*, COUNT(comments.comment_id) AS comment_count FROM articles JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id`,
+      `SELECT articles.*, COUNT(comments.comment_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id`,
       [id]
     )
     .then(({ rows }) => {
